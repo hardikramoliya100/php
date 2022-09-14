e8267e42c0fba6f7e9a3513ddb6c8e6cf7a812da
hardik ramoliya <jigneshsuvagiya10>
2022-09-14 14:34:58 +0530

    php

---
+++ hardik/MVC/controller/controller.php
@@ -0,0 +1,41 @@
+<?php
+
+class Controller{
+    public $base_url="";
+    function __construct()
+    {
+        // echo "<pre>";
+        // print_r($_SERVER);
+        $AeeofReq =explode("/",$_SERVER['REQUEST_URI']);
+        // print_r($AeeofReq);
+        // echo "<br>http://localhost/php/hardik/MVC/<br>";
+        $this->base_url = $_SERVER['REQUEST_SCHEME']."://".$_SERVER['SERVER_NAME']."/".$AeeofReq[1]."/".$AeeofReq[2]."/".$AeeofReq[3];
+        $this->base_url_asset = $this->base_url."/assets";
+        // exit;
+
+        if (isset($_SERVER['PATH_INFO'])) {
+            switch ($_SERVER['PATH_INFO']) {
+                case '/home':
+                    include_once("views/header.php");
+                    include_once("views/maincontent.php");
+                    include_once("views/footer.php");
+                    break;
+                
+                case '/about':
+                    echo "about";
+                    // include_once("views/header.php");
+                    // include_once("views/maincontent.php");
+                    // include_once("views/maincontent.php");
+                    break;
+                
+                default:
+                    # code...
+                    break;
+            }
+        }
+    }
+
+}
+
+$Controller = new Controller;
+?>
