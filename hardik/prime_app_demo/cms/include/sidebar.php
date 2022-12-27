e347c80503f9c9a0b813e383bf0b96d993d8a0ac
hardikramoliya100 <hardikramoliya100@gmail.com>
2022-12-27 18:58:47 +0530

    php

--- hardik/prime_app_demo/cms/include/sidebar.php
+++ hardik/prime_app_demo/cms/include/sidebar.php
@@ -32,7 +32,7 @@
         ?>
         <h4>Blog Categories</h4>
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-12">
                 <ul class="list-unstyled">
                     <?php
                     
@@ -44,20 +44,7 @@
                     ?>
                 </ul>
             </div>
-            <!-- /.col-lg-6 -->
-            <div class="col-lg-6">
-                <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.col-lg-6 -->
+            
         </div>
         <!-- /.row -->
     </div>
